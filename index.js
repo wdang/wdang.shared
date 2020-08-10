b7de558de760483d67a5435740e287ba49bd273f
William Dang <williamvandang@gmail.com>
2020-08-10 10:14:06 -0700

    autocompleter

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 "use strict";
 export * from './src/net.js';
+<<<<<<< HEAD
+=======
+export * from './src/util.string.js';
+export * from './src/autocompleter.js';
+>>>>>>> autocompleter
---
+++ src/autocompleter.js
@@ -0,0 +1,20 @@
+export default autocompleter = (() => {
+    const isSubstringOf = str => substr => str.toLowerCase().includes(substr.toLowerCase());
+    const containsAllLettersFrom = str => target => str.toLowerCase().split('').every(isSubstringOf(target));
+    const prefixedOrSuffixedWith = suffixOrPrefix => {
+        suffixOrPrefix = suffixOrPrefix.toLowerCase();
+        return word => {
+            word = word.toLowerCase();
+            return word.startsWith(suffixOrPrefix) || word.endsWith(suffixOrPrefix);
+        }
+    }
+    const prioritizeByPrefixThenAlpha = prefix => (a, b) => (prefixedOrSuffixedWith(prefix)(a)) ? -1 : a.localeCompare(b);
+    return data => {
+        const _data = data;
+        return userInput => {
+            userInput = userInput.toLowerCase();
+            return _data.filter(containsAllLettersFrom(userInput))
+                .sort(prioritizeByPrefixThenAlpha(userInput));
+        }
+    }
+})();
